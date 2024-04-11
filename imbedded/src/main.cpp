775bdaf087019c65821859d1b337afe1eca23ce8
tim kieboom <113353374+Tim-kieboom@users.noreply.github.com>
2024-04-11 11:40:26 +0200

    de goede formatting in main :smile:

--- imbedded/src/main.cpp
+++ imbedded/src/main.cpp
@@ -8,11 +8,11 @@
 #define PUBLISHTOPIC "data/asb1"
 #define SUBSCRIBETOPIC "asb/1"
 
-const char* BOT_ID = "1";
+const char *BOT_ID = "1";
 
-const char* ssid = SECRET_SSID;
-const char* password = SECRET_PASS;
-const char* mqtt_server = MQTT_HOST;
+const char *ssid = SECRET_SSID;
+const char *password = SECRET_PASS;
+const char *mqtt_server = MQTT_HOST;
 
 // const char* ntpServer = "pool.ntp.org";
 // const long  gmtOffset_sec = 3600;
@@ -21,7 +21,7 @@ const char* mqtt_server = MQTT_HOST;
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
+#define MSG_BUFFER_SIZE (50)
 char msg[MSG_BUFFER_SIZE];
 
 uint8_t asbServoPin = 26;
@@ -29,7 +29,8 @@ uint8_t asbLedPin = LED_BUILTIN;
 // ServoBarrier servo(asbServoPin, asbLedPin);
 auto servo = new ServoBarrier(asbServoPin, asbLedPin);
 
-void setup_wifi() {
+void setup_wifi()
+{
     delay(10);
     // We start by connecting to a WiFi network
     Serial.println();
@@ -39,7 +40,8 @@ void setup_wifi() {
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(500);
         Serial.print(".");
     }
@@ -50,23 +52,29 @@ void setup_wifi() {
     Serial.println(WiFi.localIP());
 }
 
-void printLocalTime() {
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+void printLocalTime()
+{
+    struct tm timeinfo;
+
+    if (!getLocalTime(&timeinfo))
+    {
+        Serial.println("Failed to obtain time");
+        return;
+    }
+    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 }
 
-void moveBarrier(bool pos) {
+void moveBarrier(bool pos)
+{
     pos ? servo->setLocationUp() : servo->setLocationDown();
 }
 
-
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
     char message[length + 1]; // +1 for null terminator
-    for (int i = 0; i < length; i++) {
+
+    for (int i = 0; i < length; i++)
+    {
         message[i] = (char)payload[i];
     }
     message[length] = '\0';
@@ -74,28 +82,33 @@ void callback(char* topic, byte* payload, unsigned int length) {
     // Serial.printf("Message arrived [%s] %s\n", topic, message);
 
     // expects X:Y:Z where X, Y, Z is a number
-    if (strcmp(topic, SUBSCRIBETOPIC) == 0) {
+    if (strcmp(topic, SUBSCRIBETOPIC) == 0)
+    {
         moveBarrier(atoi(&message[0]));
 
-        String returnMessage = "Barrier is " + String((message[0]- '0' == 1) ? "up" : "down");
+        String returnMessage = "Barrier is " + String((message[0] - '0' == 1) ? "up" : "down");
         client.publish(PUBLISHTOPIC, returnMessage.c_str());
         Serial.println(returnMessage);
     }
 
     // Print out the message for debugging
-    if (strcmp(message, "test") == 0) {
+    if (strcmp(message, "test") == 0)
+    {
         Serial.println("Test message received");
         client.publish(PUBLISHTOPIC, "Test message received");
     }
 }
 
-void reconnect() {
+void reconnect()
+{
     // Loop until we're reconnected
-    while (!client.connected()) {
+    while (!client.connected())
+    {
         Serial.print("Attempting MQTT connection...");
 
         // Attempt to connect
-        if (client.connect(BOT_ID, MQTT_USER, MQTT_PASS)) {
+        if (client.connect(BOT_ID, MQTT_USER, MQTT_PASS))
+        {
             Serial.println("connected");
 
             // Once connected, publish an announcement...
@@ -103,7 +116,9 @@ void reconnect() {
 
             // ... and resubscribe
             client.subscribe(SUBSCRIBETOPIC);
-        } else {
+        }
+        else
+        {
             Serial.print("failed, rc=");
             Serial.print(client.state());
             Serial.println(" try again in 5 seconds");
@@ -113,8 +128,9 @@ void reconnect() {
     }
 }
 
-void setup() {
-    pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+void setup()
+{
+    pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
     Serial.begin(115200);
 
     setup_wifi();
@@ -124,8 +140,10 @@ void setup() {
     servo->setLocationUp();
 }
 
-void loop() {
-    if (!client.connected()) {
+void loop()
+{
+    if (!client.connected())
+    {
         delay(2000);
         reconnect();
     }
