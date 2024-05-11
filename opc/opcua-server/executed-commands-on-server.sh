a9029717f280ae9a53e2aa21c5aaa0e99597d555
tim kieboom <113353374+Tim-kieboom@users.noreply.github.com>
2024-05-11 16:37:30 +0200

    updateded executed-commands-on-server

--- opc/opcua-server/executed-commands-on-server.sh
+++ opc/opcua-server/executed-commands-on-server.sh
@@ -25,9 +25,29 @@ sudo apt update
 sudo apt install python3-pip
 pip3 install opcua
 
-# to build docker image
+
+# install docker (full source="https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04")
+
+    sudo apt update
+
+    # install packages so that apt can use packages over HTTPS
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+    # add GPG key for the official Docker repository to your system
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    # add the Docker repository to APT source
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" # press ENTER to continue
+
+    # check if you are about to install from the Docker repo instead of the default Ubuntu repo
+    apt-cache policy docker-ce
+
+    # install DOCKER
+    sudo apt install docker-ce
+#end
+
+# build docker image
 sudo docker build -t opcua_server .
 
-# to run docker image
+# run docker image
 sudo docker run --rm -it -p 8100:8100 opcua_server
-
