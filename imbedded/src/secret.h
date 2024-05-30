cd85b2d4cd8c7df29f2cbf84ed146699eb7738cc
Thomas van Vliet <git@thomasvanvliet.net>
2024-05-30 09:33:16 +0200

    Changed mqtt details to the control center

--- imbedded/src/secret.h
+++ imbedded/src/secret.h
@@ -2,8 +2,8 @@
 #define SECRET_SSID "Soltergro-tunnel-demo"
 #define SECRET_PASS "34S!8#&9ya6$"
 
-#define MQTT_HOST   "192.168.137.55"
-#define MQTT_PORT   8802               // Voer hier de MQTT-broker poort in op de aangegeven plek
+#define MQTT_HOST   "10.3.141.1"
+#define MQTT_PORT   8802             // Voer hier de MQTT-broker poort in op de aangegeven plek
 #define MQTT_USER   "connectedsystems" // Vul hier de gebruikersnaam in voor de broker
 #define MQTT_PASS   "tincos"           // Vul hier het wachtwoord in voor de broker
 
