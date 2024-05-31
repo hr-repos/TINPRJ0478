58db113d064d8483a4a8977ffc0079bef32bb330
tim kieboom <113353374+Tim-kieboom@users.noreply.github.com>
2024-05-31 16:04:32 +0200

    updated the commands.sh's

--- opc/opcua-server/executed-commands-on-server.sh
+++ opc/opcua-server/executed-commands-on-server.sh
@@ -1,31 +1,3 @@
-get .net core:
-# Get Ubuntu version
-declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
-
-# Download Microsoft signing key and repository
-wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-
-# Install Microsoft signing key and repository
-sudo dpkg -i packages-microsoft-prod.deb
-
-# Clean up
-rm packages-microsoft-prod.deb
-
-# Update packages
-sudo apt update
-
-# install ASP.net core V8
-sudo apt install aspnetcore-runtime-8.0
-
-# install .net core V8
-sudo apt install dotnet-runtime-8.0
-
-# install pip and opcua
-sudo apt update
-sudo apt install python3-pip
-pip3 install opcua
-
-
 # install docker (full source="https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04")
 
     sudo apt update
